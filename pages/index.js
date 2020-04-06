5934ae0fca69c38e5e83ca2a018b010bc623c2f2
Ranatchai Chernbamrung <ranatchai@cleverse.com>
2020-04-07 03:43:05 +0700

    lock by pw

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 import { HomePage } from '../src/HomePage'
 
-const Home = props => <HomePage {...props} />
+let isAuth = typeof window !== 'undefined' && window.localStorage.getItem('isAuth')
+const Home = props => {
+  if (typeof window === 'undefined') {
+    return null
+  }
+  if (!isAuth) {
+    if (window.prompt() !== 'sila.codeforpublic') {
+      window.alert('Unauthorize !!')
+      return null
+    }
+    isAuth = true
+    localStorage.setItem('isAuth', true)
+  }
+  return <HomePage {...props} />
+}
 
 export default Home
